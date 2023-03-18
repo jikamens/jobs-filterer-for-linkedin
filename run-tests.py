3b6954e74f94af7c5cac37079d466cbe63c66e37
Jonathan Kamens <jik@kamens.us>
2023-03-18 15:31:42 -0400

    Make tests much more stable and test jobs search page

--- run-tests.py
+++ run-tests.py
@@ -8,6 +8,11 @@ import time
 import yaml
 
 config_file = "test-config.yml"
+job_selector = ".jobs-search-results__list-item, .jobs-job-board-list__item"
+title_selector = ".job-card-list__title"
+company_selector = (".job-card-container__primary-description, "
+                    ".job-card-container__company-name")
+location_selector = ".job-card-container__metadata-item"
 
 
 def main():
@@ -61,21 +66,44 @@ def run_tests(config, driver):
         mfa_field.send_keys(mfa_code)
         driver.find_element(By.ID, "two-step-submit-button").click()
 
+    test_job_on_page(driver, "https://www.linkedin.com/jobs",
+                     linkedin_window_handle, options_window_handle)
+    test_job_on_page(driver, "https://www.linkedin.com/jobs/search/"
+                     "keywords=Quality%20Assurance%20Engineer",
+                     linkedin_window_handle, options_window_handle)
+
+
+def test_job_on_page(driver, url,
+                     linkedin_window_handle, options_window_handle):
     # Can we find and hide a job?
-    driver.get("https://www.linkedin.com/jobs")
-    first_job = wait_for(
-        lambda: driver.find_element(By.CSS_SELECTOR,
-                                    ".jobs-job-board-list__item"))
-    job_title = wait_for(
-        lambda: first_job.find_element(By.CSS_SELECTOR,
-                                       ".job-card-list__title"))
-    job_company = first_job.find_element(
-        By.CSS_SELECTOR,
-        ".job-card-container__primary-description, "
-        ".job-card-container__company-name")
-    job_location = first_job.find_element(By.CSS_SELECTOR,
-                                          ".job-card-container__metadata-item")
-    hide_button = find_hide_button(driver, first_job)
+    driver.switch_to.window(linkedin_window_handle)
+    driver.get(url)
+    # We need to wait until we see the first job _title_ before we look for
+    # the first _job_ because empty jobs show up temporarily on the page and
+    # are then replaced by the real ones with contents (like job title).
+    titles = wait_for(lambda: driver.find_elements(
+        By.CSS_SELECTOR, title_selector))
+
+    # Wait for the page to "settle", i.e., wait until two seconds go by with
+    # the set of job title elements not changing.
+    start = time.time()
+    while time.time() - start < 2:
+        new_titles = wait_for(lambda: driver.find_elements(
+            By.CSS_SELECTOR, title_selector))
+        if titles != new_titles:
+            titles = new_titles
+            start = time.time()
+
+    first_job = wait_for(lambda: driver.find_elements(
+        By.CSS_SELECTOR, job_selector))[0]
+    job_title = wait_for(lambda: first_job.find_elements(
+        By.CSS_SELECTOR, title_selector))[0].text
+    job_company = wait_for(lambda: first_job.find_elements(
+        By.CSS_SELECTOR, company_selector))[0].text
+    job_location = wait_for(lambda: first_job.find_elements(
+        By.CSS_SELECTOR, location_selector))[0].text
+    time.sleep(1)  # Make sure the extension has had time to wire the button.
+    hide_button = wait_for(lambda: find_hide_button(driver, first_job))
     hide_button.click()
 
     # Is the job we just hid now in the options?
@@ -84,11 +112,10 @@ def run_tests(config, driver):
     wait_for(lambda: driver.find_element(By.ID, "jobs").
              get_attribute("value") == job_text)
 
-    # Can we find an unhide the same job?
+    # Can we find and unhide the same job?
     driver.switch_to.window(linkedin_window_handle)
-    first_job = driver.find_element(By.CSS_SELECTOR,
-                                    ".jobs-job-board-list__item")
-    unhide_button = wait_for(lambda: find_unhide_button(driver, first_job))
+    time.sleep(1)  # Make sure the extension has had time to wire the button.
+    unhide_button = wait_for(lambda: find_unhide_button(driver, driver))
     unhide_button.click()
 
     # Does the unhidden job get removed from the options page?
@@ -97,38 +124,34 @@ def run_tests(config, driver):
              get_attribute("value") == "")
 
 
-def wait_for(func, value=None):
+def wait_for(func):
     sleep_for = 0.1
     start = time.time()
     limit = 10
     while time.time() - start < limit:
-        try:
-            result = func()
-            if value is not None and result != value:
-                raise Exception("Nope")
-        except Exception:
-            time.sleep(sleep_for)
-            sleep_for *= 2
-        else:
+        result = func()
+        if result:
             return result
+        time.sleep(sleep_for)
+        sleep_for *= 2
     raise Exception("Timeout")
 
 
 def find_hide_button(driver, elt):
-    elts = driver.find_elements(By.TAG_NAME, "button")
+    elts = elt.find_elements(By.TAG_NAME, "button")
     for elt in elts:
         label = elt.get_attribute("aria-label")
         if label and ("Hide" in label or "Dismiss" in label):
             return elt
-    raise Exception("Couldn't find hide button")
+    return None
 
 
 def find_unhide_button(driver, elt):
-    elts = driver.find_elements(By.TAG_NAME, "button")
+    elts = elt.find_elements(By.TAG_NAME, "button")
     for elt in elts:
         if elt.text == "Undo":
             return elt
-    raise Exception("Couldn't find unhide button")
+    return None
 
 
 if __name__ == '__main__':
